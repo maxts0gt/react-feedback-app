502a3378da4b4ced5052e0c6232c36fcd967137a
Max <bazarsadtsogtjargal@gmail.com>
2022-01-03 22:07:46 +0900

    add feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
 	const [text, setText] = useState('');
 	const [rating, setRating] = useState(10);
 	const [message, setMessage] = useState('Hello');
@@ -24,9 +24,22 @@ function FeedbackForm() {
 		setText(event.target.value);
 	};
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (text.trim().length > 10) {
+			const newFeedback = {
+				text,
+				rating,
+			};
+
+			handleAdd(newFeedback);
+			setText('');
+		}
+	};
+
 	return (
 		<Card reverse={false}>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h2>How would you rate your service with us?</h2>
 				<RatingSelect select={(rating) => setRating(rating)} />
 				<div className='input-group'>
