652f05bb78f4cf08131d100001771e3f5b724279
Max <bazarsadtsogtjargal@gmail.com>
2022-01-03 21:02:45 +0900

    added Card and refactored

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,25 @@
+import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
+
 function FeedbackItem({ item }) {
+	const handleClick = (id) => {
+		console.log(id);
+	};
+
 	return (
-		<div className='card'>
+		<Card reverse={true}>
 			<div className='num-display'>{item.rating} </div>
+			<button onClick={() => handleClick(item.id)} className='close'>
+				<FaTimes color='purple' />
+			</button>
 			<div className='text-disp'>{item.text}</div>
-		</div>
+		</Card>
 	);
 }
+
+FeedbackItem.propTypes = {
+	item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
