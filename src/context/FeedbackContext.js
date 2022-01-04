6bd66df50b13906861a7bf8cd42095cf78bfd3f9
Max <bazarsadtsogtjargal@gmail.com>
2022-01-04 18:41:13 +0900

    update & delete from json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -26,8 +26,9 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Delete feedback
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
+			await fetch(`/feedback/${id}`, { method: 'DELETE' });
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
@@ -47,10 +48,21 @@ export const FeedbackProvider = ({ children }) => {
 		setFeedback([data, ...feedback]);
 	};
 
-	const updateFeedback = (id, updateItem) => {
+	// Update feedback item
+	const updateFeedback = async (id, updItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updItem),
+		});
+
+		const data = await response.json();
+
 		setFeedback(
 			feedback.map((item) =>
-				item.id === id ? { ...item, ...updateItem } : item
+				item.id === id ? { ...item, ...data } : item
 			)
 		);
 	};
