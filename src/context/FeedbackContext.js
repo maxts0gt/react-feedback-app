25c1261859993df3b7a4b3dcd74d80813c85af7d
Max <bazarsadtsogtjargal@gmail.com>
2022-01-04 18:49:27 +0900

    Revert "add feedback & set a proxy" done This reverts commit 448506a153d8358a2d463caece4671d0db60da68.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -18,7 +19,9 @@ export const FeedbackProvider = ({ children }) => {
 
 	// Fetch feedback
 	const fetchFeedback = async () => {
-		const response = await fetch(`/feedback?_sort=id&_order=desc`);
+		const response = await fetch(
+			`http://localhost:5000/feedback?_sort=id&_order=desc`
+		);
 		const data = await response.json();
 
 		setFeedback(data);
@@ -33,18 +36,9 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Add feedback
-	const addFeedback = async (newFeedback) => {
-		const response = await fetch('/feedback', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(newFeedback),
-		});
-
-		const data = await response.json();
-
-		setFeedback([data, ...feedback]);
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedback([newFeedback, ...feedback]);
 	};
 
 	const updateFeedback = (id, updateItem) => {
